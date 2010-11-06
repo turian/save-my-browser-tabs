09b6c4de4871a022e130a205532eb06bb19b0331
AllSeeingEye <nobodyhere@yandex.ru>
2010-11-06 18:29:08 +0300

    Extension base

---
+++ chrome/content/overlay.js
@@ -0,0 +1,13 @@
+var savemytabs = {
+
+	init: function()
+	{
+	},
+
+	uninit: function()
+	{
+	}
+};
+
+window.addEventListener("load",   function() { savemytabs.init();  }, false);
+window.addEventListener("unload", function() { savemytabs.uninit() }, false);
---
+++ defaults/preferences/savemytabs.js
@@ -0,0 +1,1 @@
+pref("extensions.savemytabs.period", 15); /* Period, in minutes, the state of tabs is saved */
